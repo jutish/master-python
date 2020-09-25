8463019e961340d7a1b899c917d558cb9100419f
Marcelloni Esteban Eduardo <emarcelloni@agdcorp.com.ar>
2020-09-25 18:10:22 -0300

    Aprendiendo Django

---
+++ 26-django/AprendiendoDjango/AprendiendoDjango/urls.py
@@ -0,0 +1,31 @@
+"""AprendiendoDjango URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+#Importo mi app con mis vistas
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    path('inicio/',views.index,name='inicio'),
+    path('pagina/',views.pagina,name='pagina'),
+    path('pagina/<int:rdg>',views.pagina,name='pagina_rdg'),
+    path('hola_mundo/',views.hola_mundo,name='hola_mundo'),
+    path('contacto/',views.contacto,name='contacto'),
+    path('contacto/<str:nombre>',views.contacto,name='contacto'),
+    path('contacto/<str:nombre>/<str:apellido>',views.contacto,name='contacto')
+]
---
+++ 26-django/AprendiendoDjango/myapp/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+
+# Create your views here.
+
+# Diferencia entre MVC y MVT
+# MVC: Modelo vista controlador
+# MVT: Modelo vista template - En Django la Vista hace de Controlador. Es este fichero!! y el Template es lo que en otros frameworks es la vista 
+
+
+layout = """
+<h1>Sitio Web - Esteban Marcelloni</h1>
+<hr/>
+<ul>
+	<li><a href="/inicio">Inicio</a></li>
+	<li><a href="/hola_mundo">Hola Mundo</a></li>
+	<li><a href="/pagina">Pagina de Prueba</a></li>
+	<li><a href="/contacto">Contacto</a></li>
+</ul>
+<hr/>		
+"""
+
+def index(request):
+	
+	html = "<h1>Inicio</h1>"
+	html +="<h2>Años pares desde el 2020 al 2050</h2>"
+	html +="<ul>"
+	cont = 2020
+	while cont <= 2050:
+		if cont % 2 ==0:
+			html += f"<li>Año: {cont}</li>"
+		cont+=1
+	html +="</ul>"	
+	return HttpResponse(layout+html)
+
+#El request se debe pasar a cada uno de los metodos
+def hola_mundo(request):
+	return HttpResponse(layout+"""
+		<h1>Hola Mundo con Django!!</h1>
+		<h3>Soy Esteban Marcelloni</h3>
+	""")
+
+def pagina(request,rdg=0):
+	if rdg==1:
+		# return redirect('/contacto/Gavilan/Stefoni') #Aca uso la ruta
+		return redirect('contacto',nombre='Gavi', apellido='Marcelloni') #Aca uso el name= definido en la ruta.
+
+	return HttpResponse(layout+"""
+		<h1>Pagina de mi Web</h1>
+	""")
+
+def contacto(request,nombre="",apellido=""):
+	return HttpResponse(layout+f"<h2>Contacto {nombre} {apellido}</h2>")
